ce6056bb102f55be134bd44fa0749c59e1c93b70
ralvescosta <rafael.rac.mg@gmail.com>
2022-01-24 07:59:37 -0300

    feat: try to rm before mv

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::process::Command;
+use std::str::FromStr;
 
 const INPUT_DIRECTORY: &str = "/input/";
 const OUTPUT_DIRECTORY: &str = "/output/";
@@ -9,25 +10,36 @@ fn main() {
 
     for dir in input_dirs {
         let current_path = dir.unwrap().path().display().to_string();
-
         let mut zip_path = current_path.clone();
         zip_path.push_str(".zip");
 
-        let spawn = Command::new("zip")
+        let zip_command_spawn = Command::new("zip")
             .arg("-r")
             .arg(zip_path.clone())
             .arg(current_path)
             .spawn();
-
-        match spawn.unwrap().wait() {
+        match zip_command_spawn.unwrap().wait() {
             Ok(_) => {
-                Command::new("mv")
-                    .arg(zip_path)
-                    .arg(OUTPUT_DIRECTORY)
-                    .spawn()
-                    .expect("some error occur while move the zip file");
+                let expected_zip_folder = zip_path.split("/").last().unwrap();
+                let mut expected_zipped_path = String::from_str(OUTPUT_DIRECTORY).unwrap();
+                expected_zipped_path.push_str(expected_zip_folder);
+
+                let remove_command_spawn = Command::new("rm")
+                    .arg("-r")
+                    .arg(expected_zipped_path)
+                    .spawn();
+                match remove_command_spawn.unwrap().wait() {
+                    Ok(_) => {
+                        Command::new("mv")
+                            .arg(zip_path)
+                            .arg(OUTPUT_DIRECTORY)
+                            .spawn()
+                            .expect("[Err] - some error occur while move the zip");
+                    }
+                    Err(e) => println!("[Err] - error while try to perform rm command - {:?}", e),
+                }
             }
-            Err(e) => println!("{:?}", e),
+            Err(e) => println!("[Err] - error while zip folder - {:?}", e),
         }
     }
 }
